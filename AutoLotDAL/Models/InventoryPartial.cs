e447626d6668baf65512a2ce1364e08f6770e8c8
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-03 22:47:22 -0400

    Add unmapped MakeColor field to InventoryPartial

--- AutoLotDAL/Models/InventoryPartial.cs
+++ AutoLotDAL/Models/InventoryPartial.cs
@@ -1,10 +1,15 @@
-﻿namespace AutoLotConsoleApp.EF
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutoLotConsoleApp.EF
 {
     public partial class Inventory
     {
+        [NotMapped]
+        public string MakeColor => $"{Make} ({Color})";
         public override string ToString()
         {
             return $"{PetName ?? "** No Name **"} is a {Color} {Make} with ID {CarId}.";
         }
+
     }
 }
