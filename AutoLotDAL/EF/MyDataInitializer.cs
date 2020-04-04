557568ff1bc03db923c2ae2476ac9440de6990a6
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-03 23:22:02 -0400

    Add MyDataInitializer class to EF folder.

---
+++ AutoLotDAL/EF/MyDataInitializer.cs
@@ -0,0 +1,13 @@
+﻿using AutoLotConsoleApp.EF;
+using System.Data.Entity;
+
+namespace AutoLotDAL.EF
+{
+    class MyDataInitializer : DropCreateDatabaseAlways<AutoLotEntities>
+    {
+        protected override void Seed(AutoLotEntities context)
+        {
+            base.Seed(context);
+        }
+    }
+}
