7c65ebdbc906c484d3cfb931cbda1afbdb0d6d93
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-05 08:58:47 -0400

    Add index to CreditRisk class

--- AutoLotDAL/Models/CreditRisk.cs
+++ AutoLotDAL/Models/CreditRisk.cs
@@ -2,13 +2,16 @@ namespace AutoLotConsoleApp.EF
 {
     using AutoLotDAL.Models.Base;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public partial class CreditRisk : EntityBase
     {
         [StringLength(50)]
+        [Index("IDX_CreditRisk_Name",IsUnique = true, Order=2)]
         public string FirstName { get; set; }
 
         [StringLength(50)]
+        [Index("IDX_CreditRisk_Name", IsUnique = true, Order = 1)]
         public string LastName { get; set; }
     }
 }
