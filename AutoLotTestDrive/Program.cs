a720539ffdd5f415f85dc8b85336a6370b4975bf
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-05 23:40:06 -0400

    Add TestConcurrency method to Program class.

--- AutoLotTestDrive/Program.cs
+++ AutoLotTestDrive/Program.cs
@@ -3,6 +3,8 @@ using AutoLotDAL.EF;
 using AutoLotDAL.Repos;
 using System;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
 
 namespace AutoLotTestDrive
 {
@@ -54,6 +56,33 @@ namespace AutoLotTestDrive
             using (var repo = new InventoryRepo())
             {
                 repo.Delete(carId, timeStamp);
+            }
+        }
+        private static void TestConcurrency()
+        {
+            var repo1 = new InventoryRepo();
+            // Use a second repo to make sure using a different context
+            var repo2 = new InventoryRepo();
+            var car1 = repo1.GetOne(1);
+            var car2 = repo2.GetOne(1);
+            car1.PetName = "NewName";
+            repo1.Save(car1);
+            car2.PetName = "OtherName";
+            try
+            {
+                repo2.Save(car2);
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                var entry = ex.Entries.Single();
+                var currentValues = entry.CurrentValues;
+                var originalValues = entry.OriginalValues;
+                var dbValues = entry.GetDatabaseValues();
+                Console.WriteLine("***** Concurrency *****");
+                Console.WriteLine("Type\tPetName");
+                Console.WriteLine($"Current:\t{currentValues[nameof(Inventory.PetName)]}");
+                Console.WriteLine($"Orig:\t{originalValues[nameof(Inventory.PetName)]}");
+                Console.WriteLine($"db:\t{dbValues[nameof(Inventory.PetName)]}");
             }
         }
     }
